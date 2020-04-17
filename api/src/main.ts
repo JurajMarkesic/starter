47974cef0a3cf19d8b5be0cb319ddde2a639ba28
Juraj Markesic <juraj.markesic.dev@gmail.com>
2020-04-17 12:52:54 +0200

    Containerized the frontend service.

--- api/src/main.ts
+++ api/src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix(process.env.APP_PREFIX || 'api');
-  await app.listen(process.env.APP_PORT || 3000);
+  app.setGlobalPrefix(process.env.API_PREFIX || 'api');
+  await app.listen(process.env.API_PORT || 3000);
 }
 bootstrap();
--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -42,7 +42,7 @@ module.exports = {
     // Doc: https://axios.nuxtjs.org/usage
     '@nuxtjs/axios',
     // Doc: https://github.com/nuxt-community/dotenv-module
-    ['@nuxtjs/dotenv', { path: '../' }]
+    '@nuxtjs/dotenv'
   ],
   /*
    ** Axios module configuration
